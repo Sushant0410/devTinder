9e6f521824dead2a8b1fd6f1d1d04c2559cdfb31
Sushant Anand <shant.anand22@gmail.com>
2025-07-17 02:18:38 +0530

    Create Feed API and pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,8 +3,9 @@ const userRouter = express.Router();
 
 const { userAuth } = require('../middlewares/auth');
 const ConnectionRequest = require('../models/connectionRequest');
+const User = require('../models/user');
 
-const USER_SAFE_DATA = "firstName, lastName, age, gender, about, skills";
+const USER_SAFE_DATA = "firstName lastName age gender about skills";
 
 
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
@@ -45,6 +46,49 @@ userRouter.get("/user/connection", userAuth, async (req, res) => {
         res.status(200).json({data});
     } catch (err) {
         res.sendStatus(400).send("ERROR: " + err.message);
+    }
+ });
+
+ userRouter.get("/feed", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+
+        limit = limit > 50 ? 50 : limit;
+
+        const skip = (page - 1) * limit;
+
+        const connectioRequest = await ConnectionRequest.find({
+            $or: [
+                {fromUserId: loggedInUser._id},
+                {toUserId: loggedInUser._id}
+            ]
+        }).select("fromUserId toUserId");
+
+        const hideUsersFromFeed = new Set();
+
+        connectioRequest.forEach((req) => {
+            hideUsersFromFeed.add(req.fromUserId.toString());
+            hideUsersFromFeed.add(req.toUserId.toString());
+        });
+
+        const users = await User.find({
+            $and: [
+                {_id: { $nin: Array.from(hideUsersFromFeed) }},
+                {_id: { $ne: loggedInUser._id }},
+            ],
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit);
+
+        if (!users) {
+            throw new Error("No user found!");
+        }
+
+        res.send(users);
+
+    } catch (err) {
+        res.status(400).json({message: err.message});
     }
  })
 
